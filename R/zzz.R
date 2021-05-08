13e6f99f3a3980b2423e66c563a6975f4dd606c7
adamkocsis <adam.t.kocsis@gmail.com>
2021-05-08 22:14:48 +0200

    Corrected position of zzz.R. Should be ready for initial build.

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+#' R interface for the GPlates Web Service and Desktop Application 
+#'
+#' Query functions to the GPlates <https://www.gplates.org/> desktop application and the GPlates Web Service <https://gws.gplates.org/> allow users to reconstruct coordinates, static plates, and Spatial objects without leaving the R running environment. This R extension was supported by the FAU GeoZentrum Nordbayern and is developed under the umbrella of the DFG (Deutsche Forschungsgemeinschaft) Research Unit TERSANE2 (For 2332, TEmperature Related Stressors in ANcient Extinctions).
+#' 
+#' This is still the Beta version. As is R, this is free software and comes with ABSOLUTELY NO WARRANTY. Nevertheless, notes about found bugs and suggestions are more than welcome. 
+#'
+#' @author Adam T. Kocsis (adam.t.kocsis@gmail.com) and Nussaibah B. Raja
+#' @docType package
+#' @name rgplates
+NULL
+
+#' @import sp
+#' @importFrom methods new 
+#' @importFrom utils flush.console sessionInfo
+NULL
+
