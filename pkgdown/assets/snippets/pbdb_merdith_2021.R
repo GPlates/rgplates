f5267b75b31418524e642e3e8102e582690debe9
Ádám T. Kocsis <adam.kocsis@fau.de>
2024-08-17 09:00:48 +0200

    Added snippet.

---
+++ pkgdown/assets/snippets/pbdb_merdith_2021.R
@@ -0,0 +1,18 @@
+# Paleobiology Database coords with Merdith et al 2021. plate model
+library(rgplates)
+
+# rgplates accessing the GPlates Web Service (70Ma reconstruction)
+plates70 <- reconstruct("static_polygons", age=70, model="MERDITH2021")
+modern70 <- reconstruct("coastlines", age=70, model="MERDITH2021")
+# plotting downloaded data (WGS84 long-lat)
+plot(plates70$geometry, border=NA, col="gray90")
+plot(modern70$geometry, border=NA, col="gray60", add=TRUE)
+
+# Maastrichtian Dinosaurs with PBDB API
+pbdb <- read.csv(paste0(
+  "https://paleobiodb.org/data1.2/occs/list.csv?",
+  "base_name=Dinosauria&interval=Maastrichtian&show=coords"))
+paleocoords<-reconstruct(
+  pbdb[, c("lng", "lat")], age= 70, model="MERDITH2021")
+points(paleocoords, pch=21, col="darkred", bg="white", cex=0.7)
+
