b95448f01e4bb3c3d7f84de8064d6d3f498ca0ed
Ádám T. Kocsis <adam.kocsis@fau.de>
2024-08-17 10:31:25 +0200

    cli-11878-742

--- snippets/pbdb_merdith_2021.R
+++ snippets/pbdb_merdith_2021.R
@@ -1,10 +1,12 @@
 # Paleobiology Database coords with Merdith et al 2021. plate model
+# Internet connection is required!
+# install.packages(c("rgplates", "httr2", "geojsonsf"))
 library(rgplates)
 
 # rgplates accessing the GPlates Web Service (70Ma reconstruction)
 plates70 <- reconstruct("static_polygons", age=70, model="MERDITH2021")
 modern70 <- reconstruct("coastlines", age=70, model="MERDITH2021")
-# plotting downloaded data (WGS84 long-lat)
+# plotting downloaded data (with sf, WGS84 long-lat)
 plot(plates70$geometry, border=NA, col="gray90")
 plot(modern70$geometry, border=NA, col="gray60", add=TRUE)
 
@@ -14,5 +16,5 @@ pbdb <- read.csv(paste0(
   "base_name=Dinosauria&interval=Maastrichtian&show=coords"))
 paleocoords<-reconstruct(
   pbdb[, c("lng", "lat")], age= 70, model="MERDITH2021")
-points(paleocoords, pch=21, col="darkred", bg="white", cex=0.7)
+points(paleocoords, pch=21, col="darkblue", bg="white", cex=0.5)
 
