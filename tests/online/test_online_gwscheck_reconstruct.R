e58c6b7a0f10b48fe965658d34d3ef5e4da5d664
adamkocsis <adam.t.kocsis@gmail.com>
2024-09-03 11:34:42 +0200

    Added unit test for checking gws-valid entries.

---
+++ tests/online/test_online_gwscheck_reconstruct.R
@@ -0,0 +1,18 @@
+# Unit tests to check that gws entries can be reconstrcuted propoerliy
+library(rgplates)
+data(gws)
+
+# a valid age
+midAge <- (gws$from+gws$to)/2
+
+# Valid entries:
+# loop through all feature/model combinations
+for(i in 1:nrow(gws)){
+	expect_silent(one <- reconstruct(x=gws$feature[i], model=gws$model[i], age=midAge[i]))
+	expect_true(inherits(one, "sf"))
+}
+
+# Invalid entries - beyond valid window
+for(i in 1:nrow(gws)){
+	expect_error(one <- reconstruct(x=gws$feature[i], model=gws$model[i], age=gws$from[i]+1))
+}
